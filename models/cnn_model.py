b85e35c672acca862d0afe885ab127eba46e966e
RafiShaik-AI <rafigiet@gmail.com>
2026-03-17 14:23:37 +0530

    Create cnn_model.py

---
+++ models/cnn_model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def create_cnn_model():
+
+    model = models.Sequential([
+        layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)),
+        layers.MaxPooling2D((2,2)),
+
+        layers.Conv2D(64,(3,3),activation='relu'),
+        layers.MaxPooling2D((2,2)),
+
+        layers.Conv2D(64,(3,3),activation='relu'),
+
+        layers.Flatten(),
+        layers.Dense(64,activation='relu'),
+        layers.Dense(10)
+    ])
+
+    return model
